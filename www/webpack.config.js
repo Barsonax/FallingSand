7cc3daf649930203f61f4fe8c4ae64fef3801cf7
Rick <rick-van-dam@outlook.com>
2019-06-10 10:07:55 +0200

    renamed output to bundle.js

--- www/webpack.config.js
+++ www/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bootstrap.js",
+    filename: "bundle.js",
   },
   mode: "development",
   plugins: [
